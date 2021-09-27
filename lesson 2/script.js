0084aba6c93bd6ca1b7f5c31f3405da471519704
Ostap <flaminis000111@gmail.com>
2021-09-27 21:05:11 +0300

    update

--- lesson 2/script.js
+++ lesson 2/script.js
@@ -1,13 +1,22 @@
-let numX = parseInt(prompt('Write a number from 0 - 100'));
-let numM = parseInt(prompt('Write a number from 0 - 100'));
+let numX = parseInt(prompt('Write a number X from 0 - 100'));
+let numM = parseInt(prompt('Write a number M from 0 - 100'));
 
-let skipEven = confirm('Skip even?');
+if(numX == '' || numM == '' || isNaN(numX) || isNaN(numM)|| numX == null || numM == null){
+    alert('Try again')
+    document.location.reload()
+}else{
+    let skipEven = confirm('Skip even?');
 
-let result = null;
-for(let i = numX; i <= numM; i++){
+    let result = null;
+    for(let i = numX; i <= numM; i++){
         if(skipEven && !(i % 2)){
             continue;  
         }
-    result += i;
+        result += i;
 };
-console.log(result);
+alert(result);
+}
+
+
+
+
