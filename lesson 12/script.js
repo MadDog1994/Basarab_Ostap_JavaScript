7b6bf63a28c5d154ab563384f2bb7de473e982bc
Ostap <flaminis000111@gmail.com>
2021-11-04 21:54:24 +0200

    update

--- lesson 12/script.js
+++ lesson 12/script.js
@@ -1,14 +1,16 @@
+const inputLength = +prompt('Input length')
 async function getRandomChinese(length){
     let count = 0;
     let sign = '';
+    console.log("In progress...");
     while(count < length){
         sign += String.fromCharCode(Date.now().toString().slice(-5));
         await new Promise((resolve,reject) => {
-            setTimeout(resolve,50)
+            setTimeout(resolve,100)
         })
         count++
     }
     console.log(sign);
+    console.log("Done");
 }
-
-console.log(getRandomChinese(4));
+getRandomChinese(inputLength)
